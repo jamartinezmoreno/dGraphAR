5d27f94ee11286c9cc72aa1d5611e5868bb1a07f
jamartinezmoreno <andres_rstudio@vm-r-package-dev.europe-west4-a.c.mdml-experimental.internal>
2023-07-24 13:18:21 +0000

    Uploading the function <rawRules>

---
+++ R/rawRules.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param datTrans An object of type transaction
+#'
+#' @return to be written
+#' @export
+#'
+#' @examples
+#' to be written
+#'
+rawRules <- function(datTrans, minNSupport, confidence, minLen, maxLen){
+
+  setDTthreads(1)
+  options(digits = 10)
+  set.seed(1234)
+
+  freqSupport <- minNSupport / dim(datTrans)[1]
+
+  rules <- arules::apriori(toSee, parameter = list(supp = freqSupport,
+                                                   conf = confidence,
+                                                   minlen = minLen,
+                                                   maxlen = maxLen,
+                                                   maxtime = 840))
+
+  output <- data.table::data.table(arules::inspect(rules))
+  return(output)
+}
