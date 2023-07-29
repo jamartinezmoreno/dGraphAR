656f3f9f0da42e48a42481e862e16013d2f5cdbd
jamartinezmoreno <andres_rstudio@vm-r-package-dev.europe-west4-a.c.mdml-experimental.internal>
2023-07-29 10:58:42 +0000

    code cleaning

--- R/plotFreqItems.R
+++ R/plotFreqItems.R
@@ -24,22 +24,22 @@ plotFreqItems <- function(datFreq, topN){
 
 
   output <- ggplot2::ggplot(data=datTAB_toPlot, ggplot2::aes(x=label, y=freq)) +
-    ggplot2::geom_bar(stat="identity", fill="dodgerblue4", alpha=0.8) +
-    ggplot2::scale_fill_manual(values=ggplot2::alpha(c("dodgerblue4"), 0.8)) +
-    ggplot2::scale_y_continuous(limits = c(0, 0.8), labels = scales::percent) +
-    ggplot2::geom_text(ggplot2::aes(x=label, y=freq, label=cases),
-                       size=6,
-                       vjust = -0.5,
-                       position = ggplot2::position_dodge(width=1)) +
-    ggplot2::xlab("\nItems") +
-    ggplot2::ylab("Frequency\n") +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(plot.title=ggplot2::element_text(size=24),
-                   legend.position="none",
-                   axis.text=ggplot2::element_text(size=18),
-                   axis.text.x=ggplot2::element_text(angle = 45, vjust = 1, hjust=1),
-                   axis.title.y=ggplot2::element_text(size=22),
-                   axis.title.x=ggplot2::element_text(size=22))
+            ggplot2::geom_bar(stat="identity", fill="dodgerblue4", alpha=0.8) +
+            ggplot2::scale_fill_manual(values=ggplot2::alpha(c("dodgerblue4"), 0.8)) +
+            ggplot2::scale_y_continuous(limits = c(0, 0.8), labels = scales::percent) +
+            ggplot2::geom_text(ggplot2::aes(x=label, y=freq, label=cases),
+                               size=6,
+                               vjust = -0.5,
+                               position = ggplot2::position_dodge(width=1)) +
+            ggplot2::xlab("\nItems") +
+            ggplot2::ylab("Frequency\n") +
+            ggplot2::theme_minimal() +
+            ggplot2::theme(plot.title=ggplot2::element_text(size=24),
+                           legend.position="none",
+                           axis.text=ggplot2::element_text(size=18),
+                           axis.text.x=ggplot2::element_text(angle = 45, vjust = 1, hjust=1),
+                           axis.title.y=ggplot2::element_text(size=22),
+                           axis.title.x=ggplot2::element_text(size=22))
   return(output)
 }
 
