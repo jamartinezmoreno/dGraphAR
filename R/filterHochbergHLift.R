6737276ab5123f1358c17835957c7b01b47f2c34
jamartinezmoreno <andres_rstudio@vm-r-package-dev.europe-west4-a.c.mdml-experimental.internal>
2023-07-30 09:44:22 +0000

    The function <filterHochbergHLift> has been added

---
+++ R/filterHochbergHLift.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param datTrans An object of type transaction
+#'
+#' @return to be written
+#' @export
+#'
+#' @examples
+#' to be written
+#'
+filterHochbergHLift <- function(hyperlifSignObj, minHLift){
+
+  signRules <- hyperlifSignObj$dt_rules[fisher_test_pvalue < hyperlifSignObj$signifLevel & bonferrCorrected_signif == TRUE]
+
+  setorder(signRules, fisher_test_pvalue)
+  signRules[, j_Hochberg:=1:.N]
+
+  n_rules <- nrow(signRules)
+
+  signRules[, value_j_Hochberg:=(j_Hochberg*hyperlifSignObj$signifLevel )/n_rules, by=c("j_Hochberg")]
+  signRules[, idx_Hochberg:= fisher_test_pvalue <= value_j_Hochberg, by=c("j_Hochberg")]
+
+  output <- signRules[signRules$idx_Hochberg & hyperLift >= minHLift, !c("bonferrCorrected_signif", "j_Hochberg", "idx_Hochberg"), with = FALSE]
+
+  return(output)
+}
+
